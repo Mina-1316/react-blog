3e384cc3b15a50a68ad4fda3ea1beaa9b42fc931
Mina-1316 <mina1316.canreply@gmail.com>
2022-08-01 18:30:05 +0900

    make example link to each webpage and make markdown document

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+      </ul>
     </div>
   );
 }
